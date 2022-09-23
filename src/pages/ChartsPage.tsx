efe97814c2daff5d45b3f303aa45081af1b523bb
Vick Kumar <Vick.Kumar44@gmail.com>
2022-09-23 18:37:51 -0400

    created charts

--- src/pages/ChartsPage.tsx
+++ src/pages/ChartsPage.tsx
@@ -1,7 +1,35 @@
+import { useState, useEffect, useRef } from 'react';
+import * as d3 from 'd3';
+
 export const ChartsPage = () => {
+  const [data , _ ] = useState([24, 30, 45, 60, 20, 65, 75]);
+  const svgRef = useRef(null);
+
+  useEffect(() => {
+    //set up the svg
+    const w = 400;
+    const h = 100;
+    const svg = d3.select(svgRef.current)
+      .attr('width', w)
+      .attr('height', h)
+      .style('background-color', '#d3d3d3');
+      //set the scales
+    const xScale = d3.scaleLinear()
+      .domain([0, data.length - 1])
+      .range([0, w]);
+    const yScale = d3.scaleLinear()
+      .domain([0, d3.max(data)])
+      .range([0, h]);
+    //set the line
+
+
+
+  }, [data]);
+
   return (
     <div>
-      <h1>Remote Charts Page</h1>
+      <h1>Remote Charts Page using D3</h1>
+      <svg ref={svgRef} />
     </div>
   );
 };
