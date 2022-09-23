a21b90095797d20cc13ca7ce502b89a008dc8f88
Vick Kumar <Vick.Kumar44@gmail.com>
2022-09-23 18:49:06 -0400

    added null

--- src/pages/ChartsPage.tsx
+++ src/pages/ChartsPage.tsx
@@ -3,7 +3,7 @@ import * as d3 from 'd3';
 
 export const ChartsPage = () => {
   const [data] = useState([24, 30, 45, 60, 20, 65, 75]);
-  const svgRef = useRef();
+  const svgRef = useRef(null);
 
   useEffect(() => {
     //set up the svg
@@ -36,13 +36,12 @@ export const ChartsPage = () => {
       .attr('d', value => line(value))
       .attr('fill', 'none')
       .attr('stroke', 'blue');
-      
-
   }, [data]);
 
   return (
     <div>
       <h1>Remote Charts Page using D3</h1>
+      /
       <svg ref={svgRef} />
     </div>
   );
